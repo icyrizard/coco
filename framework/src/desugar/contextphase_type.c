7b3d3e3c99beffb8788968f2bc69112ad6838462
Richard Torenvliet - Home pc <rich.riz@gmail.com>
2013-03-18 08:37:06 +0100

    worked on type checking, just a checkpoint

--- framework/src/desugar/contextphase_type.c
+++ framework/src/desugar/contextphase_type.c
@@ -11,7 +11,7 @@
 /* INFO object containing two linked list.
  */
 struct INFO {
-    node *head;
+    type *t;
 };
 
 /* MakeInfo allocates a new info 'object'
@@ -36,16 +36,42 @@ static info *FreeInfo( info *info)
 }
 
 node *TYPEassign(node *arg_node, node *arg_info){
+    DBUG_ENTER ("TYPEassign");
     type t_expected = VARDEC_TYPE(VAR_DECL(ASSIGN_LET(arg_node)));
+    TRAVdo(ASSIGN_EXPR(arg_node));
+    DBUG_RETURN(arg_node);
+}
 
-
+void type_to_string(type t, char *type_str){
+     switch (VARDEC_TYPE( arg_node)) {
+        case TYPE_bool:
+            type_str = "bool";
+            break;
+        case TYPE_int:
+            type_str = "int";
+            break;
+        case TYPE_float:
+            type_str = "float";
+            break;
+        case TYPE_void:
+            type_str = "void";
+            break;
+    }
 }
 
 node *TYPEvar(node *arg_node, info *arg_info){
-     VAR_DECL
+    DBUG_ENTER("TYPEvar");
+    char *type_str;
+    /* get type of vardec */
+    type t = VARDEC_TYPE(VAR_DECL(arg_node));
 
+    /* compare to expected type */
+    if(t != arg_info->t){
+        DBUG_ASSERT(0, "type mismatch %s and %s",
+                type_to_string(t, type_str), type_to_string(arg_info->t, type_str);
+    }
 
-
+    DBUG_RETURN(arg_node);
 }
 
 

085130903cfaac08788544c63452c5425cf8343e
Rik van der Kooij <rikvanderkooij@gmail.com>
2013-03-20 21:39:37 +0100

    Global decs param are now not added in local variable definition list

--- framework/src/context/link_variables.h
+++ framework/src/context/link_variables.h
@@ -3,6 +3,7 @@ extern node *CTPdoLinkVar(node *syntaxtree);
 extern node *LVARglobaldec( node *arg_node, info *arg_info);
 extern node *LVARglobaldef( node *arg_node, info *arg_info);
 extern node *LVARfundef( node *arg_node, info *arg_info);
+extern node *LVARfundec( node *arg_node, info *arg_info);
 extern node *LVARfunheader( node *arg_node, info *arg_info);
 extern node *LVARparam( node *arg_node, info *arg_info);
 extern node *LVARfuncall( node *arg_node, info *arg_info);

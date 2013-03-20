2119ab903e3370d2f9fe2fb00745f15671ab470e
Rik van der Kooij <rikvanderkooij@gmail.com>
2013-03-20 01:49:26 +0100

    finished type checking

--- framework/src/context/type_check.c
+++ framework/src/context/type_check.c
@@ -9,7 +9,8 @@
 #include "ctinfo.h"
 #include "list_hash.h"
 
-/* arrays to convert types into strings */
+/* arrays for easy conversion of type
+ * numbers into strings */
 char* monops[3] = { "not", "neg", "unknown" };
 char* binops[14] = { "add", "sub", "mul", "div", "modulo", "less than",
                      "less equals", "greater than", "greater equals", "equals",
@@ -88,14 +89,37 @@ type get_decl_type(node *decl)
     }
 }
 
+void check_argument_type(node *exprlist, node *paramlist, info *arg_info)
+{
+    type t_expr, t_param;
+    int i = 1;
+
+    /* go through list and check types */
+    while(exprlist) {
+        TRAVdo(EXPRLIST_HEAD(exprlist), arg_info);
+        t_expr = arg_info->t;
+        t_param = PARAM_TYPE(PARAMLIST_HEAD(paramlist));
+
+        if(t_expr != t_param)
+            CTIerror(":%d: error: argument %d is of type '%s' but expected type '%s'",
+                    NODE_LINE(EXPRLIST_HEAD(exprlist)), i, types[t_expr], types[t_param]);
+        exprlist = EXPRLIST_NEXT(exprlist);
+        paramlist = PARAMLIST_NEXT(paramlist);
+        i++;
+    }
+    return;
+}
+
 /*********************   Traverse   *********************/
 node *TYPEfundef(node *arg_node, info *arg_info)
 {
     type rtn_def, rtn_expr;
     DBUG_ENTER("TYPEfundef");
 
+    /* traverse header and get return type */
     TRAVdo(FUNDEF_HEADER(arg_node), arg_info);
     rtn_def = arg_info->t;
+
 
     if(FUNDEF_BODY(arg_node) == NULL) {
         rtn_expr = TYPE_void;
@@ -159,9 +183,12 @@ node *TYPEfuncall(node *arg_node, info *arg_info)
             CTIerror(":%d: error: '%s' takes %d parameters, but 1 argument is passed", NODE_LINE(arg_node), funname, num_params);
         else
             CTIerror(":%d: error: '%s' takes %d parameters, but %d arguments are passed", NODE_LINE(arg_node), funname, num_params, num_args);
+
+        arg_info->t = TYPE_unknown;
     }
 
-    /* TODO check argument type */
+    /* check argument type */
+    check_argument_type(FUNCALL_ARGUMENTS(arg_node), FUNHEADER_PARAMS(VAR_DECL(FUNCALL_ID(arg_node))), arg_info);
 
     /* set type in info to return type */
     arg_info->t = FUNHEADER_RETTYPE(VAR_DECL(FUNCALL_ID(arg_node)));
@@ -180,61 +207,77 @@ node *TYPEbinop(node *arg_node, info *arg_info)
     TRAVdo( BINOP_LEFT(arg_node), arg_info);
     left = arg_info->t;
 
+    if(left == TYPE_unknown)
+        DBUG_RETURN(arg_node);
+
     /* get the type of right expression */
     TRAVdo( BINOP_RIGHT(arg_node), arg_info);
-    right= arg_info->t;
+    right = arg_info->t;
+
+    if(right == TYPE_unknown)
+        DBUG_RETURN(arg_node);
 
     /* check type of left and right correspond with the operation */
     switch(BINOP_OP(arg_node)) {
         case BO_add:
         case BO_mul:
             /* left and right must be same type */
-            if(left != right)
+            if(left != right) {
                 CTIerror(":%d: error: invalid operand type to operator", NODE_LINE(arg_node));
-            else
+                arg_info->t = TYPE_unknown;
+            } else
                 arg_info->t = left;
             break;
         case BO_sub:
         case BO_div:
             /* left and right cannot be bool and must be same type */
-            if(left != right || left == TYPE_bool || right == TYPE_bool)
+            if(left != right || left == TYPE_bool || right == TYPE_bool) {
                 CTIerror(":%d: error: invalid operand type to operator", NODE_LINE(arg_node));
-            else
+                arg_info->t = TYPE_unknown;
+            } else
                 arg_info->t = left;
             break;
         case BO_mod:
             /* left and right must be int */
-            if(left != TYPE_int || right != TYPE_int)
+            if(left != TYPE_int || right != TYPE_int) {
                 CTIerror(":%d: error: both sides of the modulo operator must be of integer type", NODE_LINE(arg_node));
-            else
+                arg_info->t = TYPE_unknown;
+            } else
                 arg_info->t = TYPE_int;
             break;
         case BO_lt:
         case BO_le:
         case BO_gt:
         case BO_ge:
+            /* left and right must be same type */
+            if(left == TYPE_bool || right == TYPE_bool) {
+                CTIerror(":%d: error: can only compare arithmetic types "\
+                         "with this comparison", NODE_LINE(arg_node));
+                arg_info->t = TYPE_unknown;
+            } else if(left != right) {
+                    CTIerror(":%d: error: cannot compare expression of type "
+                            "'%s' with expression of type '%s'",
+                            NODE_LINE(arg_node), types[left], types[right]);
+                arg_info->t = TYPE_unknown;
+            } else
+                arg_info->t = TYPE_bool;
+            break;
         case BO_eq:
         case BO_ne:
-            /* left and right must be same type */
-            if(left == TYPE_bool || right == TYPE_bool)
-                CTIerror(":%d: error: can only compare arithmetic types"\
-                         "with this comparison", NODE_LINE(arg_node));
-            else if(left != right)
-                if(left == TYPE_float)
-                    CTIerror(":%d: error: cannot compare expression of type 'float' with expression of type 'int'",
-                            NODE_LINE(arg_node));
-                else
-                    CTIerror(":%d: error: cannot compare expression of type 'int' with expression of type 'float'",
-                            NODE_LINE(arg_node));
-            else
-                arg_info->t = TYPE_bool;
+            if(left != right) {
+                CTIerror(":%d: error: cannot compare expression of type "
+                        "'%s' with expression of type '%s'",
+                        NODE_LINE(arg_node), types[left], types[right]);
+                arg_info->t = TYPE_unknown;
+            }
             break;
         case BO_or:
         case BO_and:
             /* left and right must be bool */
-            if(left != TYPE_bool || right != TYPE_bool)
+            if(left != TYPE_bool || right != TYPE_bool) {
                 CTIerror(":%d: error: both sides of a boolean operator must be boolean expressions");
-            else
+                arg_info->t = TYPE_unknown;
+            } else
                 arg_info->t = TYPE_bool;
             break;
         default:
@@ -242,6 +285,36 @@ node *TYPEbinop(node *arg_node, info *arg_info)
     }
     DBUG_RETURN(arg_node);
 }
+
+node *TYPEmonop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("TYPEmonop");
+
+    TRAVdo(MONOP_RIGHT(arg_node), arg_info);
+
+    switch(MONOP_OP(arg_node))
+    {
+        case MO_not:
+            if(arg_info->t != TYPE_bool) {
+                CTIerror(":%d: error: logical negation is only defined for booleans",
+                        NODE_LINE(arg_node));
+                arg_info->t = TYPE_unknown;
+            }
+            break;
+        case MO_neg:
+            if(arg_info->t == TYPE_bool) {
+                CTIerror(":%d: error: unary minus is only defined for arithmetic types",
+                        NODE_LINE(arg_node));
+                arg_info->t = TYPE_unknown;
+            }
+            break;
+        case MO_unknown:
+            break;
+    }
+
+    DBUG_RETURN(arg_node);
+}
+
 
 node *TYPEvar(node *arg_node, info *arg_info)
 {
@@ -281,14 +354,17 @@ node *TYPEbool(node *arg_node, info *arg_info)
 
 node *TYPEassign(node *arg_node, info *arg_info)
 {
-    type varlet;//, expr;
+    type varlet;
     DBUG_ENTER("TYPEassign");
 
+    /* get type of varlet */
     varlet = get_decl_type(VAR_DECL(ASSIGN_LET(arg_node)));
 
+    /* check if expr is of same type as varlet */
     TRAVdo(ASSIGN_EXPR(arg_node), arg_info);
 
-    if(varlet != arg_info->t)
+    if(varlet != TYPE_unknown && arg_info->t != TYPE_unknown &&
+            varlet != arg_info->t)
         CTIerror(":%d: error: cannot initialize '%s', of type '%s', with an "
                  "expression of type '%s'", NODE_LINE(arg_node),
                  VAR_NAME(ASSIGN_LET(arg_node)), types[varlet], types[arg_info->t]);
@@ -300,11 +376,76 @@ node *TYPEconditionif(node *arg_node, info *arg_info)
 {
     DBUG_ENTER("TYPEconditionif");
 
+    /* check if expr is of type bool */
     TRAVdo(CONDITIONIF_EXPR(arg_node), arg_info);
+
+    if(arg_info->t != TYPE_unknown && arg_info->t != TYPE_bool)
+        CTIerror(":%d: error: conditional expression must be of 'bool' type",
+                NODE_LINE(CONDITIONIF_EXPR(arg_node)));
+
+    /* type check loop condition body and optional else block */
+    TRAVdo(CONDITIONIF_BLOCK(arg_node), arg_info);
+    TRAVopt(CONDITIONIF_ELSEBLOCK(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *TYPEwhileloop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("TYPEwhileloop");
+
+    /* check if expr is of type bool */
+    TRAVdo(WHILELOOP_EXPR(arg_node), arg_info);
 
     if(arg_info->t != TYPE_bool)
         CTIerror(":%d: error: conditional expression must be of 'bool' type",
-                NODE_LINE(CONDITIONIF_EXPR(arg_node)));
+                NODE_LINE(WHILELOOP_EXPR(arg_node)));
+
+    /* type check the loop body */
+    TRAVdo(WHILELOOP_BLOCK(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *TYPEcast(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("TYPEcast");
+
+    TRAVdo(CAST_RIGHT(arg_node), arg_info);
+
+    /* check on casting void types */
+    if(arg_info->t == TYPE_void) {
+        CTIerror(":%d: error: can only cast between basic types",
+            NODE_LINE(arg_node));
+        arg_info->t = TYPE_unknown;
+    }
+
+    DBUG_RETURN(arg_node);
+}
+
+node *TYPEforloop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("TYPEforloop");
+
+    /* check forloop variable initialization expr on int type */
+    TRAVdo(ASSIGN_EXPR(FORLOOP_STARTVALUE(arg_node)), arg_info);
+    if(arg_info->t != TYPE_int)
+        CTIerror(":%d: error: expression must be of type 'int'",
+                NODE_LINE(ASSIGN_EXPR(FORLOOP_STARTVALUE(arg_node))));
+
+    /* check stop value expr on int type */
+    TRAVdo(FORLOOP_STOPVALUE(arg_node), arg_info);
+    if(arg_info->t != TYPE_int)
+        CTIerror(":%d: error: expression must be of type 'int'",
+                NODE_LINE(FORLOOP_STOPVALUE(arg_node)));
+
+    /* check step value expr on int type */
+    if(FORLOOP_STEPVALUE(arg_node) != NULL) {
+        TRAVdo(FORLOOP_STEPVALUE(arg_node), arg_info);
+        if(arg_info->t != TYPE_int)
+            CTIerror(":%d: error: expression must be of type 'int'",
+                NODE_LINE(ASSIGN_EXPR(FORLOOP_STOPVALUE(arg_node))));
+    }
 
     DBUG_RETURN(arg_node);
 }
@@ -313,7 +454,7 @@ node *CTPdoType(node *syntaxtree)
 {
     info *info;
 
-    DBUG_ENTER("CTPdoType");
+    DBUG_ENTER("TYPEdoType");
 
     DBUG_ASSERT( ( syntaxtree != NULL), "CTPdoType called with empty syntaxtree");
 
